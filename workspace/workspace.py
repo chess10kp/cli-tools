1d16e99480ae4ad07cccd403e7bd159ec4642468
Nitin <nitinshankarmadhu@gmail.com>
2022-11-09 17:31:43 -0500

    Added script runner

--- workspace/workspace.py
+++ workspace/workspace.py
@@ -9,22 +9,34 @@ except ModuleNotFoundError:
     click.echo("pip install click sqlite3 sys webbrowser")
     quit()
 
+browser_path = "/usr/bin/brave"
+class Workspace:
+    def __init__(self):
+        f_web = os.path.dirname(getsourcefile(lambda : 0)) + "/urls.txt"
+        f_app = os.path.dirname(getsourcefile(lambda : 0)) + "/apps.txt"
+        f_scripts = os.path.dirname(getsourcefile(lambda : 0)) +"/scripts.txt"
+
+    def open_web(self):
+        pass
+
 
 @click.command()
-def hello():
+def init_workspace():
     try:
         with open(os.path.dirname(getsourcefile(lambda : 0))+"/urls.txt") as f:
             f = f.read().split()
-            for i in f:
-                try:
-                    webbrowser.get('/usr/bin/firefox').open(i)
-                except webbrowser.Error:
-                    webbrowser.open(i)
+            browser_obj = webbrowser.get('firefox')
+            if len(f)>0:
+                for i in f:
+                    browser_obj.open_new_tab(i)
+            else:
+                subprocess.Popen(browser_path)
+
     except FileNotFoundError:
         click.echo("url file does not exist")
 
     try:
-        with open(os.path.dirname(getsourcefile(lambda :0))+"/apps.txt") as f:
+        with open(os.path.dirname(getsourcefile(lambda: 0))+"/apps.txt") as f:
             f = f.read().split()
             for i in f:
                 subprocess.Popen(i)
@@ -32,14 +44,14 @@ def hello():
         click.echo("apps file does not exist")
 
     try:
-        with open(os.path.dirname(getsourcefile(lambda :0))+"/scripts.txt") as f:
-            f = f.read().split()
-            for i in f:
-                subprocess.Popen(i)
+        f = open(os.path.dirname(getsourcefile(lambda: 0))+"/scripts.txt")
+        fr = f.read()
+        for i in fr:
+            subprocess.check_output(i)
     except FileNotFoundError:
-        click.echo("Scripts.txt may have been moved")
+        click.echo("scripts.txt may have been moved")
 
     quit()
 
 if __name__ == "__main__":
-    hello()
+    init_workspace()
